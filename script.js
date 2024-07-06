69b9ca60ae593d44df4c950c7594349880c33b28
Syed <sm1708h@greenwich.ac.uk>
2024-07-06 22:15:02 +0100

    InitialCommit

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch('https://restcountries.com/v3.1/all').then((res) => res.json()).then((data)=> {
+    data.forEach((country) => {
+        console.log(data)
+        const countriesContainer = document.querySelector('.countries-container')
+const countryCard = document.createElement('a')
+countryCard.classList.add('country-card')
+countryCard.href = `/country.html?name=${country.name.common}`
+
+
+countryCard.innerHTML = 
+`
+<img src="${country.flags.svg}" alt="${country.name.common} flag">
+<div class="card-text">
+    <h3 class="card-title">${country.name.common}</h3>
+    <p><b>Population: </b> ${country.population.toLocaleString('en-IN')}</p>
+    <p><b>Region: </b>${country.region}</p>
+    <p><b>Capital: </b>${country.capital?.[0]}</p>
+</div>
+`
+
+countriesContainer.append(countryCard)
+
+    })
+})
+
+
+
+
+// const cardImg = document.createElement('img')
+// cardImg.src = 'https://flagcdn.com/is.svg'
+// countryCard.append(cardImg)
+
+
