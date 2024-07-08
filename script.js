e1439ba6cf35e70bfad6ac9e8f416181c949a21f
Syed <sm1708h@greenwich.ac.uk>
2024-07-08 11:12:55 +0100

    Filter regions, prettier fix, node modules added.

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-fetch('https://restcountries.com/v3.1/all').then((res) => res.json()).then((data)=> {
+const filterByRegion = document.querySelector(".filter-by-region");
+const countriesContainer = document.querySelector(".countries-container");
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
     data.forEach((country) => {
-        console.log(data)
-        const countriesContainer = document.querySelector('.countries-container')
-const countryCard = document.createElement('a')
-countryCard.classList.add('country-card')
-countryCard.href = `/country.html?name=${country.name.common}`
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country-card");
+      countryCard.href = `/country.html?name=${country.name.common}`;
 
-
-countryCard.innerHTML = 
-`
+      countryCard.innerHTML = `
 <img src="${country.flags.svg}" alt="${country.name.common} flag">
 <div class="card-text">
     <h3 class="card-title">${country.name.common}</h3>
-    <p><b>Population: </b> ${country.population.toLocaleString('en-IN')}</p>
+    <p><b>Population: </b> ${country.population.toLocaleString("en-IN")}</p>
     <p><b>Region: </b>${country.region}</p>
     <p><b>Capital: </b>${country.capital?.[0]}</p>
 </div>
-`
+`;
 
-countriesContainer.append(countryCard)
+      countriesContainer.append(countryCard);
+    });
+  });
 
-    })
-})
+filterByRegion.addEventListener("change", (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      countriesContainer.innerHTML = "";
+      data.forEach((country) => {
+        const countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `/country.html?name=${country.name.common}`;
 
+        countryCard.innerHTML = `
+<img src="${country.flags.svg}" alt="${country.name.common} flag">
+<div class="card-text">
+    <h3 class="card-title">${country.name.common}</h3>
+    <p><b>Population: </b> ${country.population.toLocaleString("en-IN")}</p>
+    <p><b>Region: </b>${country.region}</p>
+    <p><b>Capital: </b>${country.capital?.[0]}</p>
+</div>
+`;
 
-
-
-// const cardImg = document.createElement('img')
-// cardImg.src = 'https://flagcdn.com/is.svg'
-// countryCard.append(cardImg)
-
-
+        countriesContainer.append(countryCard);
+      });
+    });
+});
